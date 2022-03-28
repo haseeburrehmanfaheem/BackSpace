9158f51dd0a5afa234c1fe5263b4823e02057281
Haseeb-ur-Rehman Faheem <23100044@lums.edu.pk>
2022-03-28 22:35:31 +0500

    dummy signup page added

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
+// import 'dart:js';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:backspace/pages/loginpage.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -12,7 +15,7 @@ Widget buildAuthScreen() {
   return const Text("Authenticated");
 }
 
-Scaffold BuildUnauthScreen() {
+Scaffold BuildUnauthScreen(BuildContext context) {
   return Scaffold(
     body: Container(
       child: Center(
@@ -39,6 +42,9 @@ Scaffold BuildUnauthScreen() {
               ),
             ),
             GestureDetector(
+              // onTap: () {
+              //   _navigateToNextScreen();
+              // },
               child: Container(
                 width: 380,
                 height: 60,
@@ -51,7 +57,12 @@ Scaffold BuildUnauthScreen() {
             ),
             SizedBox(height: 40),
             GestureDetector(
-              onTap: () => print("hello world"),
+              // onTap: () => print("hello world"),
+              onTap: () {
+                print("Container clicked");
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => SignupPage()));
+              },
               child: Container(
                 width: 380,
                 height: 60,
@@ -69,11 +80,45 @@ Scaffold BuildUnauthScreen() {
   );
 }
 
+// void _navigateToNextScreen(BuildContext context) {
+//   Navigator.of(context)
+//       .push(MaterialPageRoute(builder: (context) => NewScreen()));
+// }
+
+// class NewScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('New Screen')),
+//       body: Center(
+//         child: Text(
+//           'This is a new screen',
+//           style: TextStyle(fontSize: 24.0),
+//         ),
+//       ),
+//     );
+//   }
+// }
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('New Screen')),
+      body: Center(
+        child: Text(
+          'This is a new screen',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
+}
+
 class _HomeState extends State<Home> {
   @override
   bool isauth = false;
-  Widget build(BuildContext contxt) {
-    return isauth ? buildAuthScreen() : BuildUnauthScreen();
+  Widget build(BuildContext context) {
+    return isauth ? buildAuthScreen() : BuildUnauthScreen(context);
   }
 }
 //   @override
@@ -82,3 +127,155 @@ class _HomeState extends State<Home> {
 //     throw UnimplementedError();
 //   }
 // }
+
+class SignupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      // resizeToAvoidBottomPadding: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
+            )),
+      ),
+      body: SafeArea(
+        // child: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Create an Account with your LUMS email",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Column(
+                      children: [
+                        makeInput(label: "Email"),
+                        makeInput(label: "Name"),
+                        makeInput(label: "Password", obsureText: true),
+                        makeInput(label: "Confirm Pasword", obsureText: true)
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Container(
+                      padding: EdgeInsets.only(top: 3, left: 3),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40),
+                          border: Border(
+                              bottom: BorderSide(color: Colors.black),
+                              top: BorderSide(color: Colors.black),
+                              right: BorderSide(color: Colors.black),
+                              left: BorderSide(color: Colors.black))),
+                      child: MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+                        onPressed: () {},
+                        color: Colors.black,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40)),
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Already have an account? "),
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          // ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget makeInput({label, obsureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        label,
+        style: TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      SizedBox(
+        height: 5,
+      ),
+      TextField(
+        obscureText: obsureText,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+          border:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+        ),
+      ),
+      SizedBox(
+        height: 30,
+      )
+    ],
+  );
+}
