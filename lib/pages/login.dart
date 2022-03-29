fcbd987cd37a13ffb5081ac726033c19e240ff25
Haseeb-ur-Rehman Faheem <23100044@lums.edu.pk>
2022-03-30 01:03:04 +0500

    login+signup page done(missing firebase auth)

---
+++ lib/pages/login.dart
@@ -0,0 +1,178 @@
+import 'package:backspace/pages/signup.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'homepage.dart';
+
+class Login extends StatefulWidget {
+  Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  GlobalKey<FormState> formkey1 = GlobalKey<FormState>();
+  GlobalKey<FormState> formkey2 = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  // final String email;
+  // final String password;
+  // _LoginState(this.email, this.password);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // key: formkey,
+      resizeToAvoidBottomInset: false,
+      // resizeToAvoidBottomPadding: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
+            )),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            // child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text(
+                          "Login",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Welcome back, login to continue",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Column(
+                        children: [
+                          makeInput(
+                              label: "Email",
+                              controllerObj: emailController,
+                              fk: formkey1),
+                          makeInput(
+                              label: "Password",
+                              obsureText: true,
+                              controllerObj: passwordController,
+                              fk: formkey2),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 3, left: 3),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            border: Border(
+                                bottom: BorderSide(color: Colors.black),
+                                top: BorderSide(color: Colors.black),
+                                right: BorderSide(color: Colors.black),
+                                left: BorderSide(color: Colors.black))),
+                        child: MaterialButton(
+                          minWidth: double.infinity,
+                          height: 60,
+                          onPressed: () {
+                            // print("hello world");
+                            // if()
+                            // print(nameController.text);
+                            // print(emailController.text);
+                            // print(formkey1.currentState!.validate());
+                            if (formkey1.currentState!.validate() &&
+                                formkey2.currentState!.validate()) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  backgroundColor: Colors.white,
+                                  content: Text(
+                                    'Validation Successful',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }
+                          },
+                          color: Colors.black,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40)),
+                          child: const Text(
+                            "LOGIN",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Don't have an account?"),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            textStyle: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                decoration: TextDecoration.underline),
+                            primary: Colors.black,
+                          ),
+                          onPressed: () {
+                            // print("Container clicked");
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => SignupPage()));
+                          },
+                          child: const Text('Sign up'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/signup.dart
@@ -0,0 +1,257 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+class SignupPage extends StatelessWidget {
+  GlobalKey<FormState> formkey1 = GlobalKey<FormState>();
+  GlobalKey<FormState> formkey2 = GlobalKey<FormState>();
+  GlobalKey<FormState> formkey3 = GlobalKey<FormState>();
+  GlobalKey<FormState> formkey4 = GlobalKey<FormState>();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController password1Controller = TextEditingController();
+  final TextEditingController password2Controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // key: formkey,
+      resizeToAvoidBottomInset: false,
+      // resizeToAvoidBottomPadding: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
+            )),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            // child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Create an Account with your LUMS email",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Column(
+                        children: [
+                          makeInput(
+                              label: "Email",
+                              controllerObj: emailController,
+                              fk: formkey1),
+                          makeInput(
+                              label: "Name",
+                              controllerObj: nameController,
+                              fk: formkey2),
+                          makeInput(
+                              label: "Password",
+                              obsureText: true,
+                              controllerObj: password1Controller,
+                              fk: formkey3),
+                          makeInput(
+                              label: "Confirm Password",
+                              obsureText: true,
+                              controllerObj: password2Controller,
+                              fk: formkey4,
+                              p1controller: password1Controller)
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 3, left: 3),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            border: const Border(
+                                bottom: BorderSide(color: Colors.black),
+                                top: BorderSide(color: Colors.black),
+                                right: BorderSide(color: Colors.black),
+                                left: BorderSide(color: Colors.black))),
+                        child: MaterialButton(
+                          minWidth: double.infinity,
+                          height: 60,
+                          onPressed: () {
+                            // print("hello world");
+                            // if()
+                            // print(nameController.text);
+                            // print(emailController.text);
+                            // print(formkey1.currentState!.validate());
+                            if (formkey1.currentState!.validate() &&
+                                formkey2.currentState!.validate() &&
+                                formkey3.currentState!.validate() &&
+                                formkey4.currentState!.validate()) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  backgroundColor: Colors.white,
+                                  content: Text(
+                                    'Validation Successful',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }
+                          },
+                          color: Colors.black,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40)),
+                          child: const Text(
+                            "Sign Up",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Already have an account?"),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            textStyle: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                decoration: TextDecoration.underline),
+                            primary: Colors.black,
+                          ),
+                          onPressed: () {
+                            // print("Container clicked");
+                            Navigator.pushReplacement(context,
+                                MaterialPageRoute(builder: (_) => Login()));
+                          },
+                          child: const Text('Log in'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget makeInput(
+    {label, obsureText = false, controllerObj, fk, p1controller = null}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        label,
+        style: const TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      const SizedBox(
+        height: 5,
+      ),
+      Form(
+        key: fk,
+        child: TextFormField(
+          obscureText: obsureText,
+          decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.grey,
+              ),
+            ),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+          ),
+          controller: controllerObj,
+          validator: (value) {
+            if (label == "Confirm Password") {
+              if (value == null || value.isEmpty) {
+                return 'Please enter the password again';
+              } else if (p1controller.text != value) {
+                return "Password is not same";
+              } else {
+                return null;
+              }
+            } else if (label == "Password") {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a password';
+              } else {
+                return null;
+              }
+            } else if (label == "Email") {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              } else if (value.length < 20) {
+                return "Enter a valid LUMS email";
+              } else if (value.substring(value.length - 12) != "@lums.edu.pk") {
+                return "Enter a valid LUMS email";
+              } else {
+                return null;
+              }
+            } else if (label == "Name") {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              } else {
+                return null;
+              }
+            }
+            // return null;
+          },
+          // onSaved: (value) {
+          //   controllerObj.text = value!;
+          // },
+          //  onSaved: (val)=>_password=val,
+        ),
+      ),
+      SizedBox(
+        height: 30,
+      ),
+    ],
+  );
+}
