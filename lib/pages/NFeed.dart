9999a3a1e6c8cab5986f2bcc883d806ab2c1b53c
nashrahshaukat <shaukatnashrah@gmail.com>
2022-04-09 23:13:43 +0500

    AddimagetoPost

--- lib/pages/NFeed.dart
+++ lib/pages/NFeed.dart
@@ -1,12 +1,15 @@
+import 'dart:io';
+
 import 'package:backspace/pages/Notification.dart';
 import 'package:backspace/pages/newsfeed.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:backspace/helper/demo_values.dart';
 import 'package:image_picker/image_picker.dart';
 
 import '../components/newsFeed/post-body/posts-text.dart';
 import '../components/newsFeed/post-header/user-icon-name.dart';
-
+final usersRef = FirebaseFirestore.instance.collection('UserData'); //database reference object
 class Feed extends StatelessWidget {
   const Feed({Key? key}) : super(key: key);
 
@@ -47,67 +50,97 @@ class Feed extends StatelessWidget {
           Expanded(
             child: Post(),
           ),
-          AddPostForm(),
+          // AddPost(),
         ],
       ),
     );
   }
 }
 
-class AddPostForm extends StatefulWidget {
-  const AddPostForm({Key? key}) : super(key: key);
+class timeline extends StatefulWidget{
 
-  @override
-  AddPostFormState createState() => AddPostFormState();
+  @override 
+  _timeline createState() => _timeline();
 }
 
-class AddPostFormState extends State<AddPostForm> {
-  final _formKey = GlobalKey<FormState>();
-
-  // Future pickImage() async {
-  //   ImagePicker.pickImage(source: ImageSource.camera);
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    return (Form(
-        key: _formKey,
-        child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-            child: TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Enter Text';
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                hintText: "Add Post",
-                fillColor: const Color(0xfff9f9fa),
-                filled: true,
-                suffixIcon: Row(
-                  mainAxisAlignment:
-                      MainAxisAlignment.spaceBetween, // added line
-                  mainAxisSize: MainAxisSize.min, // added line
-                  children: <Widget>[
-                    IconButton(
-                        onPressed: () {}, //Open Gallery here
-                        icon: const Icon(Icons.camera_alt_outlined)),
-                    IconButton(
-                        onPressed: () {}, //Open Gallery here
-                        icon: const Icon(Icons.photo)),
-                    IconButton(
-                        onPressed: () {}, //Open Gallery here
-                        icon: const Icon(Icons.arrow_forward)),
-                  ],
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-              ),
-            ))));
+class _timeline extends State<timeline> {
+  @override 
+  void initState(){
+    getUsers();
+    super.initState();
   }
+  Future<void> getUsers() async {
+    // Get docs from collection reference
+    QuerySnapshot querySnapshot = await usersRef.get();
+
+    // Get data from docs and convert map to List
+    querySnapshot.docs.forEach((doc) {print(doc.id ); });
+    //  final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+
+    // print(allData);
 }
+
+
+  @override
+   Widget build(BuildContext context){
+     return Scaffold(
+      backgroundColor: Colors.white,
+      drawer: MyDrawer(),
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
+        ),
+        title: const Text('News Feed', style: TextStyle(fontFamily: "Poppins")),
+        actions: [
+          const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Icon(Icons.search, color: Colors.black)),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: IconButton(
+                icon: const Icon(Icons.notifications_outlined),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Noti()),
+                  );
+                },
+              ))
+        ],
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+      ),
+      body: ListView(
+        children: const <Widget>[
+          Expanded(
+            child: Post(),
+          ),
+          // AddPost(),
+        ],
+      ),
+    );
+   }
+}
+
+
+
+
+// final databaseRef = FirebaseFirestore.instance.collection('UserData'); //database reference object
+// Future<void> getData() async {
+//     // Get docs from collection reference
+//     QuerySnapshot querySnapshot = await databaseRef.get();
+
+//     // Get data from docs and convert map to List
+//     final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+
+//     print(allData);
+// }
+
+
+
 
 class Post extends StatelessWidget {
   const Post({
@@ -117,7 +150,8 @@ class Post extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return 
+      Card(
       clipBehavior: Clip.antiAlias,
       child: Column(
         children: [
@@ -129,9 +163,14 @@ class Post extends StatelessWidget {
           const PostBody(postSummary: DemoValues.postSummary),
           Image.asset('assets/images/keys.jpg'),
           const PostFooter(),
+          const AddPostForm(),
+          
         ],
       ),
     );
+    
+    
+    
   }
 }
 
@@ -169,3 +208,204 @@ class _PostFooter extends State<PostFooter> {
     );
   }
 }
+
+
+class AddPostForm extends StatefulWidget {
+  const AddPostForm({Key? key}) : super(key: key);
+
+  @override
+  AddPostFormState createState() => AddPostFormState();
+}
+
+class AddPostFormState extends State<AddPostForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  // Future pickImage() async {
+  //   ImagePicker.pickImage(source: ImageSource.camera);
+  // }
+  File? file;
+  handleTakephoto() async {
+    Navigator.pop(context);
+    var file = await ImagePicker().pickImage(source: ImageSource.camera, 
+    maxHeight: 675, maxWidth: 960,);
+    setState((){
+      this.file = File(file!.path);
+    });
+
+  }
+  handleChoosefromgallery() async {
+    Navigator.pop(context);
+    var file = await ImagePicker().pickImage(source: ImageSource.gallery, 
+    maxHeight: 675, maxWidth: 960,);
+    setState((){
+      this.file = File(file!.path);
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return (Form(
+        key: _formKey,
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+            child: TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Enter Text';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                hintText: "Add Post",
+                fillColor: const Color(0xfff9f9fa),
+                filled: true,
+                suffixIcon: Row(
+                  mainAxisAlignment:
+                      MainAxisAlignment.spaceBetween, // added line
+                  mainAxisSize: MainAxisSize.min, // added line
+                  children: <Widget>[
+                    IconButton(
+                        onPressed: handleTakephoto, //Open Camera here
+                        icon: const Icon(Icons.camera_alt_outlined)),
+                    IconButton(
+                        onPressed: handleChoosefromgallery, //Open Gallery here
+                        icon: const Icon(Icons.photo)),
+                    IconButton(
+                        onPressed: () {}, //post the post
+                        icon: const Icon(Icons.arrow_forward)),
+                  ],
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+            ))));
+  }
+}
+
+
+
+
+
+
+
+
+
+// class AddPost extends StatefulWidget{
+
+//   // final User currentUser;
+
+//   // AddPost({this.currentUser });
+
+//   @override 
+//   _UploadState createState() => _UploadState();
+// }
+// class _UploadState extends State<AddPost> {
+
+//   File? file;
+
+
+//   handleTakephoto() async {
+//     Navigator.pop(context);
+//     var file = await ImagePicker().pickImage(source: ImageSource.camera, 
+//     maxHeight: 675, maxWidth: 960,);
+//     setState((){
+//       this.file = File(file!.path);
+//     });
+
+//   }
+//   handleChoosefromgallery() async {
+//     Navigator.pop(context);
+//     var file = await ImagePicker().pickImage(source: ImageSource.gallery, 
+//     maxHeight: 675, maxWidth: 960,);
+//     setState((){
+//       this.file = File(file!.path);
+//     });
+//   }
+
+//   selectImage(Parentcontext){
+//     return showDialog(
+//       context: Parentcontext,
+//       builder: (context){
+//         return SimpleDialog(
+//           title: Text("Create Post"),
+//           children: <Widget>[
+//             SimpleDialogOption(
+//               child: Text("Photo with Camera"),
+//               onPressed: handleTakephoto,
+//             ),
+//             SimpleDialogOption(
+//               child: Text("Image from gallery"),
+//               onPressed: handleChoosefromgallery,
+//             ),
+//             SimpleDialogOption(
+//               child: Text("Cancel"),
+//               onPressed: () => Navigator.pop(context),
+//             )
+//           ],
+//         );
+//       }
+
+//     );
+//   }
+
+
+//   Buildpostsplashscreen()
+//   {
+//     return Column
+//     (
+//       children: <Widget>[  
+//                 Padding(  
+//                   padding: EdgeInsets.all(15),  
+//                   child: TextField(  
+//                     decoration: InputDecoration(  
+//                       border: OutlineInputBorder(),  
+//                       hintText: 'Add post..',  
+//                     ),  
+//                   ),  
+//                 ),  
+
+//         IconButton(
+//                 icon: const Icon(Icons.camera_alt_outlined),
+//                 onPressed: () => selectImage(context)
+//               ),
+
+//       ],
+//     );
+  
+//           //     TextField(
+//           //   decoration : InputDecoration(
+//           //     hintText: "Add new post..",
+//           //     border: InputBorder.none,
+//           //     prefixIcon: Icon(Icons.camera_alt_outlined),
+
+//           //   ),
+//           // ),
+              
+//   }
+//   Container Builduploadform()
+//   {
+//     return Scaffold(
+      
+//     );
+//     // return Container(
+      
+//     //   Text('title'),
+//     //   // decoration: BoxDecoration(
+//     //   //   image: DecorationImage(
+//     //   //     fit: BoxFit.cover, image: FileImage(file!),
+//     //   //   ),
+//     //   // ),
+//     // );
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return file == null? Buildpostsplashscreen() : Builduploadform() ;
+//     // return Buildpostsplashscreen();
+//     // return Buildpostsplashscreen();
+            
+
+            
+//   }
+// }
