72728c848c8962fd533bae195be2651f77ccad9e
IbrahimSanaullah <23100204@lums.edu.pk>
2022-04-09 15:49:18 +0500

    Pixel error Fixed

--- lib/pages/EditProfile.dart
+++ lib/pages/EditProfile.dart
@@ -9,23 +9,20 @@ class EditProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.chevron_left),
-            onPressed: () => Navigator.pop(context, false),
-          ),
-          title: const Text('Edit Profile',
-              style: TextStyle(fontFamily: "Poppins")),
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.chevron_left),
+          onPressed: () => Navigator.pop(context, false),
         ),
-        body: const MyCustomForm(),
+        title:
+            const Text('Edit Profile', style: TextStyle(fontFamily: "Poppins")),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
       ),
+      body: SingleChildScrollView(child: MyCustomForm()),
     );
   }
 }
