5f683f954361e493e4a203add966a3eb93e711b0
Haseeb <23100044@lums.edu.pk>
2022-04-10 15:15:46 +0500

    fixed padding in login/signup

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:backspace/pages/login.dart';
 import 'package:backspace/pages/signup.dart';
 import 'package:flutter/cupertino.dart';
