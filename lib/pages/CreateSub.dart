292268a58326ed5c361bdba22be9d4abb90088af
IbrahimSanaullah <23100204@lums.edu.pk>
2022-04-11 21:54:37 +0500

    Create Subspace

--- lib/pages/CreateSub.dart
+++ lib/pages/CreateSub.dart
@@ -35,9 +35,99 @@ class CreateSub extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
       ),
-      body: const Center(
-        child: Text('Hello World'),
-      ),
+      body: SingleChildScrollView(child: SubspaceForm()),
+    );
+  }
+}
+
+class SubspaceForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+          child: Text(
+            "Create SubSpace",
+            style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w400,
+                color: Colors.black87),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+          child: Text(
+            "Subspace Name",
+            style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+                color: Colors.black87),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          child: TextFormField(
+            // initialValue: txt.data,
+            decoration: InputDecoration(
+              fillColor: Color(0xfff9f9fa),
+              filled: true,
+              //icon: Icon(Icons.favorite),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Color(0xff000000)),
+              ),
+            ),
+          ),
+        ),
+        const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8)),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+          child: Text(
+            "About",
+            style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+                color: Colors.black87),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          child: TextFormField(
+            // initialValue: txt.data,
+            decoration: InputDecoration(
+              fillColor: Color(0xfff9f9fa),
+              filled: true,
+              //icon: Icon(Icons.favorite),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Color(0xff000000)),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 24, top: 80, right: 24),
+          child: MaterialButton(
+            minWidth: double.infinity,
+            height: 60,
+            onPressed: () {},
+            color: Colors.black,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+            child: const Text(
+              "Create SubSpace",
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                  color: Colors.white),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
