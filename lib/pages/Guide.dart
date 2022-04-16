12f2a3bceb485d19a060ddfe22ee3aeb17be41a8
Haseeb <23100044@lums.edu.pk>
2022-04-17 01:34:16 +0500

    guide fixed

--- lib/pages/Guide.dart
+++ lib/pages/Guide.dart
@@ -13,7 +13,6 @@ import 'package:image_picker/image_picker.dart';
 import '../components/newsFeed/post-body/posts-text.dart';
 import '../components/newsFeed/post-header/user-icon-name.dart';
 
-
 class Guide extends StatelessWidget {
   const Guide({Key? key}) : super(key: key);
 
@@ -36,18 +35,37 @@ class Guide extends StatelessWidget {
         ),
         body: ListView(
           physics: BouncingScrollPhysics(),
-          children: 
-            [
-            PostCard(Username: DemoValues.userName2, Title: DemoValues.postTitle2, Summary: DemoValues.postSummary2, PostDetails: DemoValues.postdetail, PostImage: DemoValues.postImage2,),
-            PostCard(Username: DemoValues.userName3, Title: DemoValues.postTitle3, Summary: DemoValues.postSummary3, PostDetails: DemoValues.postdetail2, PostImage: DemoValues.postImage3,),
-            PostCard(Username: DemoValues.userName4, Title: DemoValues.postTitle4, Summary: DemoValues.postSummary4, PostDetails: DemoValues.postdetail3, PostImage: DemoValues.postImage4,),
-            ],
-          ), 
+          children: [
+            PostCard(
+              Username: DemoValues.userName2,
+              Title: DemoValues.postTitle2,
+              Summary: DemoValues.postSummary2,
+              PostDetails: DemoValues.postdetail,
+              PostImage:
+                  "https://firebasestorage.googleapis.com/v0/b/backspace-current.appspot.com/o/UserImages%2Farticle1.jpg?alt=media&token=d52a7b31-b60e-42df-9864-b5e50a57c52e",
+            ),
+            PostCard(
+              Username: DemoValues.userName3,
+              Title: DemoValues.postTitle3,
+              Summary: DemoValues.postSummary3,
+              PostDetails: DemoValues.postdetail2,
+              PostImage:
+                  "https://firebasestorage.googleapis.com/v0/b/backspace-current.appspot.com/o/UserImages%2Farticle2.jpg?alt=media&token=84f044d1-df54-4f37-bb0d-0bb9f8ea9102",
+            ),
+            PostCard(
+              Username: DemoValues.userName4,
+              Title: DemoValues.postTitle4,
+              Summary: DemoValues.postSummary4,
+              PostDetails: DemoValues.postdetail3,
+              PostImage:
+                  "https://firebasestorage.googleapis.com/v0/b/backspace-current.appspot.com/o/UserImages%2Farticle3.jpg?alt=media&token=98b8d2bf-743c-419d-b541-89322c71ea60",
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
-
 
 class PostCard extends StatelessWidget {
   final String Title;
@@ -56,13 +74,12 @@ class PostCard extends StatelessWidget {
   final String PostDetails;
   final String PostImage;
 
-
   const PostCard({
-  required this.Title,
-  required this.Username,
-  required this.Summary,
-  required this.PostDetails,
-  required this.PostImage,
+    required this.Title,
+    required this.Username,
+    required this.Summary,
+    required this.PostDetails,
+    required this.PostImage,
   });
 
   @override
@@ -70,22 +87,35 @@ class PostCard extends StatelessWidget {
     return AspectRatio(
       aspectRatio: 8 / 3,
       child: InkWell(
-      child: Card(
-        elevation: 2,
-        child: Container(
-          margin: const EdgeInsets.all(4.0),
-          padding: const EdgeInsets.all(4.0),
-          child: Column(
-            children: <Widget>[
-              _Post(username: Username, title: Title, summary: Summary, postDetails: PostDetails, postImage: PostImage,),
-            ],
+        child: Card(
+          elevation: 2,
+          child: Container(
+            margin: const EdgeInsets.all(4.0),
+            padding: const EdgeInsets.all(4.0),
+            child: Column(
+              children: <Widget>[
+                _Post(
+                  username: Username,
+                  title: Title,
+                  summary: Summary,
+                  postDetails: PostDetails,
+                  postImage: PostImage,
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      onTap: (){
-        Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => ArticlePost(title: Title, postDetails: PostDetails, PostImage: PostImage, username: Username,)));
-      },
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ArticlePost(
+                        title: Title,
+                        postDetails: PostDetails,
+                        PostImage: PostImage,
+                        username: Username,
+                      )));
+        },
       ),
     );
   }
@@ -99,18 +129,31 @@ class _Post extends StatelessWidget {
   final String postImage;
 
   const _Post({
-  required this.title,
-  required this.username,
-  required this.summary,
-  required this.postDetails,
-  required this.postImage,
+    required this.title,
+    required this.username,
+    required this.summary,
+    required this.postDetails,
+    required this.postImage,
   });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       flex: 3,
-      child: Row(children: <Widget>[_PostImage(postImage1: postImage, ), Padding(padding:const EdgeInsets.only(left: 6.0) ,), _PostTitleAndSummary(Username1: username, Title1: title, Summary1: summary, PostDetails1: postDetails, )]),
+      child: Row(children: <Widget>[
+        _PostImage(
+          postImage1: postImage,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 6.0),
+        ),
+        _PostTitleAndSummary(
+          Username1: username,
+          Title1: title,
+          Summary1: summary,
+          PostDetails1: postDetails,
+        )
+      ]),
     );
   }
 }
@@ -122,14 +165,11 @@ class _PostTitleAndSummary extends StatelessWidget {
   final String Summary1;
   final String PostDetails1;
 
-
-
   const _PostTitleAndSummary({
-  required this.Title1,
-  required this.Username1,
-  required this.Summary1,
-  required this.PostDetails1,
-
+    required this.Title1,
+    required this.Username1,
+    required this.Summary1,
+    required this.PostDetails1,
   });
 
   @override
@@ -149,11 +189,17 @@ class _PostTitleAndSummary extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             // Padding(padding: const EdgeInsets.only(left: 4.0),),
-            Text(Title1, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            Text(Title1,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
             SizedBox(height: 5.0),
             Text(Summary1, style: TextStyle(fontFamily: "Poppins")),
-             SizedBox(height: 3.0),
-            Text(Username1, style: TextStyle(fontFamily: "Poppins", color: Colors.grey, fontStyle: FontStyle.italic, fontSize: 13)),
+            SizedBox(height: 3.0),
+            Text(Username1,
+                style: TextStyle(
+                    fontFamily: "Poppins",
+                    color: Colors.grey,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 13)),
           ],
         ),
       ),
@@ -165,17 +211,14 @@ class _PostImage extends StatelessWidget {
   final String postImage1;
 
   const _PostImage({
-  required this.postImage1,
+    required this.postImage1,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(flex: 3, child: Image.asset(postImage1));
+    return Expanded(flex: 3, child: Image.network(postImage1));
   }
 }
-
-
-
 
 class ArticlePost extends StatelessWidget {
   final String title;
@@ -183,12 +226,11 @@ class ArticlePost extends StatelessWidget {
   final String PostImage;
   final String username;
 
-
   const ArticlePost({
-  required this.title,
-  required this.postDetails,
-  required this.PostImage,
-  required this.username,
+    required this.title,
+    required this.postDetails,
+    required this.PostImage,
+    required this.username,
   });
 
   @override
@@ -197,23 +239,25 @@ class ArticlePost extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Welcome to Flutter',
       home: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-            leading: IconButton(
-              icon: Icon(Icons.chevron_left),
-              onPressed: () => Navigator.pop(context, false),
-            ),
-            title: const Text('Freshman Guide',
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.chevron_left),
+            onPressed: () => Navigator.pop(context, false),
+          ),
+          title: const Text('Freshman Guide',
               style: TextStyle(fontFamily: "Poppins")),
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.black,
-          ),
-          body: 
-          SingleChildScrollView(child: 
-          TextArticle(title1: title, postDetails1: postDetails, PostImages: PostImage, username1: username,)
-          
-          ),
-          ),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+        body: SingleChildScrollView(
+            child: TextArticle(
+          title1: title,
+          postDetails1: postDetails,
+          PostImages: PostImage,
+          username1: username,
+        )),
+      ),
     );
   }
 }
@@ -224,23 +268,22 @@ class TextArticle extends StatelessWidget {
   final String title1;
   final String username1;
 
-
   const TextArticle({
-  required this.title1,
-  required this.postDetails1,
-  required this.PostImages,
-  required this.username1,
+    required this.title1,
+    required this.postDetails1,
+    required this.PostImages,
+    required this.username1,
   });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children:  <Widget>[
+      children: <Widget>[
         Padding(
           padding: EdgeInsets.only(left: 24, top: 44),
           child: Text(
-            title1 ,
+            title1,
             style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.w400,
@@ -250,9 +293,12 @@ class TextArticle extends StatelessWidget {
         ),
         Padding(
           padding: EdgeInsets.only(left: 24, top: 15),
-          child: Text(
-            username1, style: TextStyle(fontFamily: "Poppins", color: Colors.grey, fontStyle: FontStyle.italic, fontSize: 13)
-          ),
+          child: Text(username1,
+              style: TextStyle(
+                  fontFamily: "Poppins",
+                  color: Colors.grey,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 13)),
         ),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
@@ -270,24 +316,23 @@ class TextArticle extends StatelessWidget {
   }
 }
 
-
 class ArticleImage extends StatelessWidget {
   final String postImage2;
 
   const ArticleImage({
-  required this.postImage2,
+    required this.postImage2,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-          padding: EdgeInsets.only(left: 24, top: 24, right: 24),
-          child: Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Expanded(flex: 3, child: Image.asset(postImage2)),
-    ],
-    ),
+      padding: EdgeInsets.only(left: 24, top: 24, right: 24),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(flex: 3, child: Image.network(postImage2)),
+        ],
+      ),
     );
   }
 }
