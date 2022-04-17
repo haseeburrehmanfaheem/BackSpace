09b95aa3f53b47427b48af9b9f122850d9e15513
imzeerak <zeerakbabar@gmail.com>
2022-04-17 18:20:24 +0500

    comment box ui

--- lib/pages/NFeed.dart
+++ lib/pages/NFeed.dart
@@ -435,35 +435,53 @@ class _PostscommentState extends State<Postscomment> {
         ),
       ),
       bottomNavigationBar: BottomAppBar(
-        child: Form(
-          key: formGlobalKey,
-          child: TextFormField(
-            controller: commentController,
-            onTap: () {},
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Enter some Text';
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-              hintText: "Add Comment",
-              fillColor: const Color(0xfff9f9fa),
-              filled: true,
-              suffixIcon: IconButton(
-                icon: Icon(Icons.arrow_forward),
-                onPressed: () async {
-                  if (formGlobalKey.currentState!.validate()) {
-                    // print(commentController.text);
-                    await updateCommentInDB(
-                        commentController.text, widget.post_id);
-                    commentController.clear();
-                    // Navigator.pushReplacement(context,Postscomment(likes: widget.likes, post_id: widget.post_id, userName: widget.userName, userimage: widget.userimage, time: widget.time, PostImg: widget.PostImg, postcontent: widget.postcontent))
-                  }
-                },
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
+        child: Container(
+          margin: EdgeInsets.all(15.0),
+          child: Form(
+            key: formGlobalKey,
+            child: TextFormField(
+              controller: commentController,
+              
+              onTap: () {},
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Enter some Text';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.white,
+                       ),
+                      borderRadius: BorderRadius.circular(25.0),
+                  ),
+                hintText: "Add Comment",
+                fillColor: Colors.grey.withOpacity(0.3),
+                // fromRGBO(249, 249, 250, 1),
+                
+                filled: true,
+                isDense: true,
+                contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.arrow_forward),
+                  onPressed: () async {
+                    if (formGlobalKey.currentState!.validate()) {
+                      // print(commentController.text);
+                      await updateCommentInDB(
+                          commentController.text, widget.post_id);
+                      commentController.clear();
+                      // Navigator.pushReplacement(context,Postscomment(likes: widget.likes, post_id: widget.post_id, userName: widget.userName, userimage: widget.userimage, time: widget.time, PostImg: widget.PostImg, postcontent: widget.postcontent))
+                    }
+                  },
+                ),
+                
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                  
+                  // borderSide: BorderSide(color: Colors.white)
+                ),
               ),
             ),
           ),
